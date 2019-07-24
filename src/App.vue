d90a7a566705608fc0bf12a420ed3f875b54052a
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-07-24 16:34:02 +0530

    added Notes

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <!-- <appClient></appClient> -->
-    <appCaseInformation></appCaseInformation>
+    <appClient></appClient>
+    <!-- <appCaseInformation></appCaseInformation> -->
+    <!-- <appNotes></appNotes> -->
   </div>
 </template>
 
 <script>
-// import Client from "@/components/Client.vue";
-import CaseInformation from "@/components/CaseInformation.vue";
+import Client from "@/components/Client.vue";
+// import CaseInformation from "@/components/CaseInformation.vue";
+// import Notes from "@/components/Notes.vue";
 
 export default {
   name: "app",
   components: {
-    // appClient: Client,
-    appCaseInformation: CaseInformation
+    appClient: Client
+    // appCaseInformation: CaseInformation,
+    // appNotes:Notes
   }
 };
 </script>
---
+++ src/components/Notes.vue
@@ -0,0 +1,49 @@
+<template>
+  <div role="tablist">
+    <b-card no-body>
+      <b-card-header
+        header-tag="header"
+        header="Notes"
+        header-bg-variant="secondary"
+        header-text-variant="white"
+        v-b-toggle.accordion-1
+        class="p-1"
+        role="tab"
+      >
+      </b-card-header>
+      <b-collapse
+        id="accordion-1"
+        visible
+        accordion="my-accordion"
+        role="tabpanel"
+      >
+        <b-card-body>
+          <b-form>
+            <b-form-group
+              id="notesByCpa"
+              label="Notes by CPA"
+              label-for="notesByCpaInput"
+            >
+              <b-form-textarea
+                id="notesByCpaInput"
+                v-model="NotesForm.notesByCpa"
+              ></b-form-textarea>
+            </b-form-group>
+          </b-form>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      NotesForm: {
+        notesByCpa: ""
+      }
+    };
+  }
+};
+</script>
