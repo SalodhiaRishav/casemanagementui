59875d7850b916a6724829d0dba48cbc1fbe1345
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-07-24 16:18:51 +0530

    added case information

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <app-client></app-client>
+    <!-- <appClient></appClient> -->
+    <appCaseInformation></appCaseInformation>
   </div>
 </template>
 
 <script>
-import Client from "@/components/Client.vue";
+// import Client from "@/components/Client.vue";
+import CaseInformation from "@/components/CaseInformation.vue";
 
 export default {
   name: "app",
   components: {
-    appClient: Client
+    // appClient: Client,
+    appCaseInformation: CaseInformation
   }
 };
 </script>
---
+++ src/components/CaseInformation.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div role="tablist">
+      <b-card no-body>
+        <b-card-header
+          header-tag="header"
+          header="Client Information"
+          header-bg-variant="secondary"
+          header-text-variant="white"
+          v-b-toggle.accordion-1
+          class="p-1"
+          role="tab"
+        >
+        </b-card-header>
+        <b-collapse
+          id="accordion-1"
+          visible
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <b-form>
+              <b-form-group
+                id="description"
+                label="Description"
+                label-for="descriptionInput"
+              >
+                <b-form-textarea
+                  id="descriptionInput"
+                  v-model="caseInformation.description"
+                ></b-form-textarea>
+              </b-form-group>
+              <b-form-group
+                id="messageFromClient"
+                label="Messsage from client"
+                label-for="messageFromClientInput"
+              >
+                <b-form-textarea
+                  id="messageFromClientInput"
+                  v-model="caseInformation.messageFromClient"
+                ></b-form-textarea>
+              </b-form-group>
+              <b-form-group
+                id="priority"
+                label="Priority"
+                label-for="priorityInput"
+              >
+                <b-form-select
+                  id="priorityInput"
+                  v-model="caseInformation.priority"
+                  :options="priorities"
+                  required
+                ></b-form-select>
+              </b-form-group>
+            </b-form>
+          </b-card-body>
+        </b-collapse>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      caseInformation: {
+        description: "",
+        messageFromClient: "",
+        priority: null
+      }
+    };
+  },
+  computed: {
+    priorities: () => {
+      return [
+        { text: "Select priority", value: null },
+        "High",
+        "Medium",
+        "Low"
+      ];
+    }
+  }
+};
+</script>
