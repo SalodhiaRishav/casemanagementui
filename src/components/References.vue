46033107c6a67c4b2c80e9ee2667a468a0d442dc
Rishav Salodhia <rishav.salodhia@nagarro.com>
2019-07-25 18:06:46 +0530

    done reference right

--- src/components/References.vue
+++ src/components/References.vue
@@ -18,7 +18,7 @@
         role="tabpanel"
       >
         <b-card-body>
-          <b-table striped hover :items="references" :fields="fields">
+          <b-table fixed striped hover :items="references" :fields="fields">
             <template slot="index" slot-scope="data">
               {{ data.index + 1 }}
             </template>
@@ -26,32 +26,36 @@
               <button @click="deleteReference(data.index)">Delete</button>
             </template>
           </b-table>
-
+          <b-table fixed :fields="fields">
+            <template slot="HEAD_identity" slot-scope="data">
+              <b-form-input
+                id="identityInput"
+                :placeholder="data.label"
+                v-model="referenceForm.identity"
+              ></b-form-input>
+            </template>
+            <template slot="HEAD_type">
+              <b-form-select
+                id="referenceTypeInput"
+                :options="referenceTypes"
+                v-model="referenceForm.type"
+              ></b-form-select>
+            </template>
+            <template slot="HEAD_comment" slot-scope="data">
+              <b-form-input
+                id="commentInput"
+                :placeholder="data.label"
+                v-model="referenceForm.comment"
+              ></b-form-input>
+            </template>
+            <template slot="HEAD_index">
+              {{&nbsp;}}
+            </template>
+            <template slot="HEAD_#">
+              {{&nbsp;}}
+            </template>
+          </b-table>
           <b-button @click="addNewReference">Add</b-button>
-
-          <b-form v-if="showReferenceForm">
-            <b-row>
-              <b-col>
-                <b-form-select
-                  id="referenceTypeInput"
-                  :options="referenceTypes"
-                  v-model="referenceForm.type"
-                ></b-form-select>
-              </b-col>
-              <b-col>
-                <b-form-input
-                  id="identityInput"
-                  v-model="referenceForm.identity"
-                ></b-form-input>
-              </b-col>
-              <b-col>
-                <b-form-input
-                  id="comment"
-                  v-model="referenceForm.comment"
-                ></b-form-input>
-              </b-col>
-            </b-row>
-          </b-form>
         </b-card-body>
       </b-collapse>
     </b-card>
